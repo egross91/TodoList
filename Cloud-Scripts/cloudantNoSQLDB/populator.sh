5d6930c8798b6133060998af36bf3b74b3fe4788
Trisha Moyer <tehanlon@us.ibm.com>
2016-09-16 14:02:01 -0500

    Adding example cloudant design, the file to parse the inputs, and adding populator.sh which does the population of the database.

---
+++ Cloud-Scripts/cloudantNoSQLDB/populator.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+##
+# Copyright IBM Corporation 2016
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+# If any commands fail, we want the shell script to exit immediately.
+set -e
+
+# Set scripts folder variable
+scriptsFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "scriptsFolder: $scriptsFolder"
+
+# Parse input parameters
+source "$scriptsFolder/parse_inputs.sh"
+
+# Delete to do database just in case it already exists (we need a clean slate)
+curl -X DELETE https://$username.cloudant.com/$database -u $username:$password
+
+# Create todo database
+curl -X PUT https://$username.cloudant.com/$database -u $username:$password
+
+# Upload design document
+curl -X PUT "https://$username.cloudant.com/todo/_design/main_design" -u $username:$password -d @"$scriptsFolder/main_design.json"
+
+# Create user documents
+curl -H "Content-Type: application/json" -d @"$scriptsFolder/users.json" -X POST https://$username.cloudant.com/$database/_bulk_docs -u $username:$password
+
+echo
+echo "Finished populating cloudant database '$database'."
